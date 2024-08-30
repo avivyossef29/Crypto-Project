a7a5043009a099c0e1c29c805b4927dc8ddc4b8f
Yonatan Goldenberg <yonatan.r.gold@gmail.com>
2024-08-30 10:48:52 +0300

    work

--- POODLE/server/server.py
+++ POODLE/server/server.py
@@ -1,10 +1,24 @@
 from flask import Flask, jsonify
+import ssl
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
+@app.route('/', methods=['GET'])
 def api_root():
-    return jsonify(message="Hello from the REST API!")
+    return jsonify(message="Hello, SSLv3 with CBC encryption!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    
+    # Create an SSL context
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv3)
+
+    # # Enable SSL 3.0
+    # context.options &= ~ssl.OP_NO_SSLv3
+
+    # Load server's certificate and private key
+    context.load_cert_chain(certfile='server.crt', keyfile='server.key')
+
+    # Limit cipher suites to CBC
+    context.set_ciphers('AES128-SHA')
+
+    app.run(host='0.0.0.0', port=443, ssl_context=context)
--- POODLE/victim/connect.sh
+++ POODLE/victim/connect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Connect to the SSLv3 server using OpenSSL
-echo "Connecting to server using OpenSSL..."
-openssl s_client -connect server-demo:443 -ssl3
+# Connect to the Flask server using OpenSSL
+#echo "Connecting to server using OpenSSL..."
+#openssl s_client -connect demo-server:443 -ssl3 -cipher AES128-SHA
 
-# Connect to the SSLv3 server using Curl
+# Connect to the Flask server using Curl
 echo "Connecting to server using Curl..."
-curl --sslv3 https://server-demo/api --insecure
+curl --sslv3 --ciphers AES128-SHA https://demo-server/ --insecure
